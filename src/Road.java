b9ef319bfe57f0d568e53ec6697da522e4d7fc36
salmankarsa <salmankarsa@gmail.com>
2020-01-23 13:58:41 +0800

    Update Road.java

--- src/Road.java
+++ src/Road.java
@@ -157,3 +157,98 @@ public class Road extends JPanel {
             v.get(a).paintMe(g);
         }
     }
+    public void step() {
+        for(int i=0;i<v.size();i++) {
+            Vehicle vehicle=v.get(i);
+
+
+            if(vehicle.turn1==true & !vehicle.road.equals("in") ) {
+                vehicle.setY(vehicle.getY()+vehicle.getSpeed() );
+                vehicle.setPosition(vehicle.getY()+vehicle.getSpeed());
+                vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+
+            }
+            else if(vehicle.turn2==true & !vehicle.road.equals("in")){
+                vehicle.setY(vehicle.getY()-vehicle.getSpeed());
+                vehicle.setPosition(vehicle.getY()-vehicle.getSpeed());
+                vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+            }
+            else if(vehicle.turn3==true & !vehicle.road.equals("in")){
+                vehicle.setX(vehicle.getX()-vehicle.getSpeed());
+                vehicle.setPosition(vehicle.getX()-vehicle.getSpeed());
+                vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+            }
+            else if(vehicle.turn3==false & vehicle.turn3==false & vehicle.turn3==false & !vehicle.road.equals("in")) {
+                vehicle.setX(vehicle.getX()+vehicle.getSpeed());
+                vehicle.setPosition(vehicle.getX()+vehicle.getSpeed());
+                vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+            }
+            else {
+                if(vehicle.current==true) {
+
+                    int count=0;
+                    if(vehicle.signal.equals("s1")) {count=0;}
+                    else if(vehicle.signal.equals("s2")) {count=1;}
+                    else if(vehicle.signal.equals("s3")) {count=2;}
+                    else if(vehicle.signal.equals("s4")) {count=3;}
+                    else if(vehicle.signal.equals("s5")) {count=4;}
+                    else if(vehicle.signal.equals("s6")) {count=5;}
+                    else if(vehicle.signal.equals("s7")) {count=6;}
+                    else if(vehicle.signal.equals("s8")) {count=7;}
+                    if(s.size()!=0) {
+                        if(count==0) {
+                            s.get(count).randomLightStatus(false, false, true);
+
+                        }
+                        else {
+                            if(s.size()==5 & count==5) {s.get(count-2).randomLightStatus(true, false, false);
+                                s.get(count-1).randomLightStatus(false, false, true);}
+                            else {
+                                s.get(count-1).randomLightStatus(true, false, false);
+                                s.get(count).randomLightStatus(false, false, true);
+                            }
+
+                        }}
+                    if(vehicle.turn1==true) {
+                        vehicle.setY(vehicle.getY()+vehicle.getSpeed() );
+                        vehicle.setRoad(vehicle.getX(), vehicle.getY());
+                    }
+                    else if(vehicle.turn2==true){
+                        vehicle.setY(vehicle.getY()-vehicle.getSpeed());
+
+                        vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+                    }
+                    else if(vehicle.turn3==true ){
+                        vehicle.setX(vehicle.getX()-vehicle.getSpeed());
+                        vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+                    }
+                    else if(vehicle.turn3==false & vehicle.turn3==false & vehicle.turn3==false ) {
+                        vehicle.setX(vehicle.getX()+vehicle.getSpeed());
+                        vehicle.setRoad(vehicle.getX(), vehicle.getY());
+
+
+                    }
+                }
+                else {
+                    vehicle.setRoad(vehicle.getX(), vehicle.getY());
+                }
+
+            }
+        }
+    }
+    public void randomLightStatus(int i,boolean r,boolean y,boolean g) {
+        s.get(i).randomLightStatus(r, y, g);
+    }
+}
